1a9da5210d32c6652bdeaaa2cb66bcf57448210d
pusin819 <shibata0819haru@gmail.com>
2023-10-15 22:15:12 +0900

    add nvim

---
+++ nvim/lua/maps.lua
@@ -0,0 +1,104 @@
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+local status, telescope = pcall(require, "telescope.builtin")
+if status then
+	-- Telescope
+	map("n", "<leader>ff", telescope.find_files)
+	map("n", "<leader>fg", telescope.live_grep)
+	map("n", "<leader>fb", telescope.buffers)
+	map("n", "<leader>fh", telescope.help_tags)
+	map("n", "<leader>fs", telescope.git_status)
+	map("n", "<leader>fc", telescope.git_commits)
+else
+	print("Telescope not found")
+end
+
+-- <leader> = the space key
+
+-- Save
+map("n", "<leader>w", "<CMD>update<CR>")
+
+-- Quit
+map("n", "<leader>q", "<CMD>q<CR>")
+
+-- Exit insert mode
+map("i", "jk", "<ESC>")
+
+-- Windows
+map("n", "<leader>pv", "<CMD>vsplit<CR>")
+map("n", "<leader>ps", "<CMD>split<CR>")
+
+-- NeoTree
+map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
+map("n", "<leader>o", "<CMD>Neotree focus<CR>")
+
+-- Buffer
+map("n", "<TAB>", "<CMD>bnext<CR>")
+map("n", "<S-TAB>", "<CMD>bprevious<CR>")
+
+-- Terminal
+map("n", "<leader>th", "<CMD>ToggleTerm size=10 direction=horizontal<CR>")
+map("n", "<leader>tv", "<CMD>ToggleTerm size=80 direction=vertical<CR>")
+
+-- Markdown Preview
+map("n", "<leader>m", "<CMD>MarkdownPreview<CR>")
+map("n", "<leader>mn", "<CMD>MarkdownPreviewStop<CR>")
+
+-- Window Navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-j>", "<C-w>j")
+
+-- Resize Windows
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
+
+-- doxygen
+local opts = { noremap = true, silent = true }
+map("n", "<Leader>nf", "<CMD>lua require('neogen').generate()<CR>", opts)
+
+-- Move to previous/next
+map("n", "<A-h>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<A-l>", "<Cmd>BufferNext<CR>", opts)
+-- Re-order to previous/next
+map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+-- Goto buffer in position...
+map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+-- Pin/unpin buffer
+map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
+-- Close buffer
+map("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
+-- Sort automatically by...
+map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
+map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
+map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
